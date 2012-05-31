e2e03d87dd5dcf082bf334fd64997b532e6aab1d
Bardiel W. Thirtytwo <bardiel@bardiel.com.ar>
2012-05-30 21:46:21 -0300

    Version 0.1.1: Check device_name existance.

--- pymput.py
+++ pymput.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """
 pymput
-Version:    0.1.0
+Version:    0.1.1
 Author:     Bardiel
 License:    Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 
@@ -16,12 +16,20 @@ Features:
 
 TODO: 
 -Accept parameters (such as "Device Name")
+-Fix elem.strip on line 49 (read comment)
+
+Changelog:
+v0.1.1
++If device_name is not found on xinput list, exit(1) and print message
+
+v0.1.0
+Initial Release
 """
 from subprocess import call
 from subprocess import check_output
 
-device_name = "ImPS/2 ALPS GlidePoint"
-
+device_name = "SynPS/2 Synaptics TouchPad"
+elid = ""
 
 p = check_output(["xinput", "list"])
 f = p.split('\n')
@@ -34,11 +42,17 @@ for elem in f:
         except:
             elid = elem[elem.find("id=")+3]
 
+if elid == "":
+    print "Deivce %s not found" % (device_name)
+    exit(1)
+
 p = check_output(["xinput", "list-props", elid])
 f = p.split('\n')
 for elem in f:
     if "Device Enabled" in elem:
-        if elem.strip() == "Device Enabled (126):	0":
+        if elem.strip() == "Device Enabled (152):	0": #"(152)" is not always the same number in every machine. 
             call(["xinput", "set-prop", elid, "Device Enabled", "1"])
+            print "Device ENABLED"
         else:
             call(["xinput", "set-prop", elid, "Device Enabled", "0"])
+            print "Device DISABLED"
